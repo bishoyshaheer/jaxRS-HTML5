679bf21db59aca73ae79cbfdd681592b7c6baa24
DESKTOP-A7J5F0K\bishoy <bishoy.shaheer@gmail.com>
2018-02-28 15:36:15 +0200

    change db to mysql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
 -- You can use this file to load seed data into the database using SQL statements
-insert into MemberHTML5 (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212')
+insert into MemberHTML5 (id, name, email, phone_number) values (0, 'Bishoy Shaheer', 'brefaat@ejada.com', '01226303624')
+insert into MemberHTML5 (id, name, email, phone_number) values (1, 'John Smith', 'john.smith@mailinator.com', '2125551212')
